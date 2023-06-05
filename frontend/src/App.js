08ab131f5b42dffe3c75a28116e79a3ed7e32975
UnaiGMorchon <unaitxu79@gmail.com>
2023-06-05 20:23:29 +0200

    footer basico habra que modificar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import Web from "./components/Web";
+import AvisoLegal from "././components/AvisoLegal";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -22,61 +24,13 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/web' element={<Web />} />
+          <Route path='/avisolegal' element={<AvisoLegal />} />
         </Routes>
       </AuthContextProvider>
+
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-/* import React, { useState } from "react";
-
-import Home from "./screens/Home";
-import Login from "./screens/Login";
-
-import firebaseApp from "./firebase/credenciales";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
-
-function App() {
-  const [user, setUser] = useState(null);
-
-  async function getRol(uid) {
-    const docuRef = doc(firestore, `usuarios/${uid}`);
-    const docu = await getDoc(docuRef);
-    const infoFinal = docu.data().rol;
-    return infoFinal;
-  }
-
-  function setUserWithFirebaseAndRol(usuarioFirebase) {
-    getRol(usuarioFirebase.uid).then((rol) => {
-      const userData = {
-        uid: usuarioFirebase.uid,
-        email: usuarioFirebase.email,
-        rol: rol,
-      };
-      setUser(userData);
-      console.log("userData fianl", userData);
-    });
-  }
-
-  onAuthStateChanged(auth, (usuarioFirebase) => {
-    if (usuarioFirebase) {
-      //funcion final
-
-      if (!user) {
-        setUserWithFirebaseAndRol(usuarioFirebase);
-      }
-    } else {
-      setUser(null);
-    }
-  });
-
-  return <>{user ? <Home user={user} /> : <Login />}</>;
-}
-
-export default App;
- */
