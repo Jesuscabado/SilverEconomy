63d3bb62a9b196d1b367b447501fe5d9856e04f7
Javier <javiergonzalez13a@gmail.com>
2023-06-07 17:15:00 +0200

    Queda por revisar el editar lo demas funciona bien,tengo error con firebase que me he excedido de uso

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,12 +1,52 @@
 import React, { useState, useEffect } from 'react';
-import firebase from '../Firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc , updateDoc,deleteDoc,doc} from 'firebase/firestore';
 import { db } from '../Firebase';
 
 const Users = () => {
+  const [email, setEmail] = useState('');
+  const [rol, setRol] = useState('');
   const [usuarios, setUsuarios] = useState([]);
   const userCollectionRef = collection(db, 'usuarios');
 
+  const crearUsuario = async () => {
+    const newUsuario = {
+      email: email,
+      rol: rol,
+    };
+
+    try {
+      const docRef = await addDoc(userCollectionRef, newUsuario);
+      setUsuarios([...usuarios, { ...newUsuario, id: docRef.id }]);
+      setEmail('');
+      setRol('');
+    } catch (error) {
+      console.error('Error adding usuario: ', error);
+    }
+  };
+   
+  const editarUsuario = async (id, rol, email) => {
+    const userDoc = doc(db, "usuarios", id);
+    const updatedUsuario = {
+      email: email,
+      rol: rol,
+    };
+  
+    try {
+      await updateDoc(userDoc, updatedUsuario);
+      console.log("Usuario actualizado correctamente");
+    } catch (error) {
+      console.error("Error al actualizar el usuario:", error);
+    }
+  };
+  
+
+    const borrarUsuario = async (id) => {
+
+        const userDoc = doc(db, "usuarios", id);
+        await deleteDoc (userDoc,{rol:"borrado"});
+
+        };
+    
   useEffect(() => {
     const getUsuarios = async () => {
       const querySnapshot = await getDocs(userCollectionRef);
@@ -18,14 +58,30 @@ const Users = () => {
     };
 
     getUsuarios();
-  }, []);
+  }, [userCollectionRef]);
 
   return (
     <div>
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        type="text"
+      />
+      <input
+        placeholder="Rol"
+        value={rol}
+        onChange={(event) => setRol(event.target.value)}
+        type="text"
+      />
+
+      <button onClick={crearUsuario}>Crear Usuario</button>
       {usuarios.map((usuario) => (
         <div key={usuario.id}>
-          <h1>Email: {usuario.email}</h1>
+          <h1>Usuario: {usuario.email}</h1>
           <h1>Rol: {usuario.rol}</h1>
+          <button onClick={() => editarUsuario(usuario.id, usuario.rol, usuario.email)}>Editar</button>
+          <button onClick={() =>{borrarUsuario(usuario.id,usuario.email,usuario.rol)}   }>Eliminar</button>
         </div>
       ))}
     </div>
