adc88ca9c0e30931e074c499ed95927ffed1cddf
Javier <javiergonzalez13a@gmail.com>
2023-06-07 16:06:07 +0200

    Puedo ver los usuarios que se registran con email y rol

--- frontend/src/Firebase.jsx
+++ frontend/src/Firebase.jsx
@@ -1,14 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth";
-/* import { getAnalytics } from "firebase/analytics"; */
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { v4 } from "uuid";
-/* import { getDatabase } from "firebase/database"; */
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -21,33 +15,28 @@ const firebaseConfig = {
   measurementId: "G-GX73D1WJXW",
 };
 
-export const provider = new GoogleAuthProvider();
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Realtime Database and get a reference to the service
-/* const database = getDatabase(app);
-
-const analytics = getAnalytics(app);
- */
 // Initialize Cloud Storage and get a reference to the service
 export const storage = getStorage(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+// Initialize Firestore and get a reference to the service
+export const db = getFirestore(app);
+
 export default app;
 
 /**
- * upload a file to firebase storage service
- * @param {File} file  archivo que se quiere subir
- * @returns {Promise <string>}  url del archivo subido
+ * Upload a file to Firebase Storage service
+ * @param {File} file - archivo que se quiere subir
+ * @returns {Promise<string>} - url del archivo subido
  */
-
-// para subir archivo a storage service cualquier tipo de archivo
-
 export async function uploadFile(file) {
-  const storageRef = ref(storage, v4()); // cambiar el nombre en funcion del ususario sino todos subiran como avatar
+  const storageRef = ref(storage, v4());
   await uploadBytes(storageRef, file);
   const url = await getDownloadURL(storageRef);
   return url;
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,122 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import firebase from '../Firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../Firebase';
 
-export default function Usuarios() {
+const Users = () => {
   const [usuarios, setUsuarios] = useState([]);
-  const [email, setEmail] = useState('');
-  const [rol, setRol] = useState('');
-  const [editUserId, setEditUserId] = useState('');
+  const userCollectionRef = collection(db, 'usuarios');
 
   useEffect(() => {
-    fetchUsuarios();
+    const getUsuarios = async () => {
+      const querySnapshot = await getDocs(userCollectionRef);
+      const data = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setUsuarios(data);
+    };
+
+    getUsuarios();
   }, []);
 
-  const fetchUsuarios = async () => {
-    try {
-      const snapshot = await firebase.firestore().collection('usuarios').get();
-      const usuariosData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-      setUsuarios(usuariosData);
-    } catch (error) {
-      console.log('Error fetching usuarios:', error);
-    }
-  };
-
-  const agregarUsuario = async () => {
-    if (!email || !rol) return;
-
-    try {
-      const nuevoUsuario = { email, rol };
-      await firebase.firestore().collection('usuarios').add(nuevoUsuario);
-      setEmail('');
-      setRol('');
-      fetchUsuarios();
-    } catch (error) {
-      console.log('Error adding usuario:', error);
-    }
-  };
-
-  const eliminarUsuario = async (usuarioId) => {
-    try {
-      await firebase.firestore().collection('usuarios').doc(usuarioId).delete();
-      fetchUsuarios();
-    } catch (error) {
-      console.log('Error deleting usuario:', error);
-    }
-  };
-
-  const editarUsuario = (usuarioId) => {
-    setEditUserId(usuarioId);
-    const usuario = usuarios.find(usuario => usuario.id === usuarioId);
-    if (usuario) {
-      setEmail(usuario.email);
-      setRol(usuario.rol);
-    }
-  };
-  
-  const actualizarUsuario = async () => {
-    if (!email || !rol || !editUserId) return;
-  
-    try {
-      const usuarioActualizado = { email, rol };
-      await firebase.firestore().collection('usuarios').doc(editUserId).update(usuarioActualizado);
-      setEmail('');
-      setRol('');
-      setEditUserId('');
-      fetchUsuarios();
-    } catch (error) {
-      console.log('Error updating usuario:', error);
-    }
-  };
-  
-  const cancelarEdicion = () => {
-    setEmail('');
-    setRol('');
-    setEditUserId('');
-  };
-  
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Rol"
-        value={rol}
-        onChange={(e) => setRol(e.target.value)}
-      />
-      <button onClick={agregarUsuario}>Agregar</button>
-
-      <button onClick={eliminarUsuario}>Eliminar</button>
-
-      {editUserId ? (
-        <>
-          <button onClick={actualizarUsuario}>Guardar</button>
-          <button onClick={cancelarEdicion}>Cancelar</button>
-        </>
-      ) : (
-        <button onClick={editarUsuario}>Editar</button>
-      )}
-
-      <table>
-        <thead>
-          <tr>
-            <th>Email</th>
-            <th>Rol</th>
-          </tr>
-        </thead>
-        <tbody>
-          {usuarios.map(usuario => (
-            <tr key={usuario.id}>
-              <td>{usuario.email}</td>
-              <td>{usuario.rol}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {usuarios.map((usuario) => (
+        <div key={usuario.id}>
+          <h1>Email: {usuario.email}</h1>
+          <h1>Rol: {usuario.rol}</h1>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+export default Users;
