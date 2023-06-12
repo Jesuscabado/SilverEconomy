231f6857ffe1695e98dcd0670b4d6493b9e7d934
UnaiGMorchon <unaitxu79@gmail.com>
2023-06-12 18:26:39 +0200

    añadido profile, informes correguir editar usuario y lo de subir foto

---
+++ frontend/src/components/Entidades.jsx
@@ -0,0 +1,74 @@
+
+const CompanyInfo = () => {
+  return (
+    <div>
+      <div>
+        <h2>Aparkabisa. Centro de transportes de Bizkaia S.A.</h2>
+        <p>Dirección: Barrio El Juncal s/n, 48510 Valle de Trápaga - Trapagaran.</p>
+        <p>Teléfono: 944189420</p>
+        <p>Fax: 944386601</p>
+        <p>Web: Aparkabisa. Centro de transportes de Bizkaia S.A.</p>
+      </div>
+      <div>
+        <h2>Azpiegiturak S.A.M.P.</h2>
+        <p>Dirección: Islas Canarias 21, 2ª - 3ª 48015, Bilbao</p>
+        <p>Teléfono: 946073700</p>
+        <p>Fax: 946073638</p>
+        <p>Web: Azpiegiturak S.A.M.P.</p>
+      </div>
+      <div>
+        <h2>BEAZ centro de empresas e innovación de Bizkaia S.A.</h2>
+        <p>Dirección: Sabino Arana, 8 48013 Bilbao</p>
+        <p>Teléfono: 944239228</p>
+        <p>Fax: 944231013</p>
+        <p>Web: BEAZ centro de empresas e innovación de Bizkaia S.A.</p>
+      </div>
+      <div>
+        <h2>BOS</h2>
+        <p>Dirección: Palacio Euskalduna Jauregia Abandoibarra Etorbidea, 4 48011 Bilbao</p>
+        <p>Teléfono: +34 94 40 35 205</p>
+        <p>Fax: +34 94 40 35 115</p>
+        <p>Web: BOS</p>
+      </div>
+      <div>
+        <h2>Bizkaia Bizkaialde</h2>
+        <p>Teléfono: 944423960 / 944068785</p>
+        <p>Web: Bizkaia Bizkaialde</p>
+      </div>
+      <div>
+        <h2>Bizkaiako Basalan S.A.M.P.</h2>
+        <p>Dirección: Avenida Madariaga 1 - 1º Dpto. 9 48014, Bilbao</p>
+        <p>Teléfono: 944761955</p>
+        <p>Fax: 944472417</p>
+        <p>Web: Bizkaiako Basalan S.A.M.P.</p>
+      </div>
+      <div>
+        <h2>Bizkaikoa</h2>
+        <p>Dirección: María Diaz de Haro 11 1º 48013, Bilbao</p>
+        <p>Teléfono: 944066083</p>
+        <p>Web: Bizkaikoa</p>
+      </div>
+      <div>
+        <h2>Crowdfunding Bizkaia</h2>
+        <p>Dirección: Plaza Sagrado Corazón, 5 7ª Planta, 48011 Bilbao</p>
+        <p>Teléfono: 94 427 50 78</p>
+        <p>Web: Crowdfunding Bizkaia</p>
+      </div>
+      <div>
+        <h2>Fundación Azkue</h2>
+        <p>Dirección: Plaza Agoitz 1 48015 Bilbao, Bizkaia</p>
+        <p>Teléfono: 94 402 80 81</p>
+        <p>Fax: 94 405 24 07</p>
+        <p>Web: Fundación Azkue</p>
+      </div>
+      <div>
+        <h2>Fundación BiscayTIK</h2>
+        <p>Dirección: Zugazarte 32, 48930 Getxo</p>
+        <p>Teléfono: 946000020</p>
+        <p>Web: Fundación BiscayTIK</p>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyInfo;
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,13 +1,281 @@
-import React from "react";
 import NavbarSinTexto from "./NavbarSinTexto";
 import SideBar from "./SideBar";
-function Profile() {
+import React, { useState, useEffect } from "react";
+import {
+  collection,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  addDoc,
+  doc,
+} from "firebase/firestore";
+import { db, uploadFile } from "../Firebase";
+import "../css/Profile.css";
+
+const Users = () => {
+  const [usuarios, setUsuarios] = useState([]);
+  const [editingUserId, setEditingUserId] = useState(null);
+  const [selectedFile, setSelectedFile] = useState({});
+  const [uploadedImages, setUploadedImages] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [userData, setUserData] = useState({
+    nombre: "",
+    apellido: "",
+    fechaNacimiento: "",
+    sexo: "",
+    rol: "",
+  });
+  const userCollectionRef = collection(db, "usuarios");
+  const crearUsuario = async () => {
+    if (isUserDataIncomplete()) {
+      alert("Por favor, completa todos los campos");
+      return;
+    }
+    try {
+      const docRef = await addDoc(userCollectionRef, userData);
+      setUsuarios([...usuarios, { ...userData, id: docRef.id }]);
+      resetUserData();
+    } catch (error) {
+      console.error("Error adding usuario: ", error);
+    }
+  };
+  /* const signup = async (email, password, rol, nombre, apellido, fechaNacimiento, sexo) => {
+    try {
+      const { user } = await createUserWithEmailAndPassword(auth, email, password);
+      const firestore = getFirestore();
+      const userDocRef = doc(firestore, 'usuarios', user.id);
+      await setDoc(userDocRef, { email, rol, nombre, apellido, fechaNacimiento, sexo });
+      console.log("signup", email, password, rol, nombre, apellido, fechaNacimiento, sexo);
+    } catch (error) {
+      console.error('Error en el registro: ', error);
+    }
+  }; */
+  const editarUsuario = async (id) => {
+    try {
+      const { email, ...updatedUserData } = userData;
+      await updateDoc(doc(userCollectionRef, id), updatedUserData);
+      setEditingUserId(null);
+      resetUserData();
+    } catch (error) {
+      console.error("Error al editar usuario: ", error);
+    }
+  };
+  const borrarUsuario = async (id) => {
+    try {
+      const userDoc = doc(db, "usuarios", id);
+      await deleteDoc(userDoc);
+      setUsuarios((prevUsuarios) =>
+        prevUsuarios.filter((usuario) => usuario.id !== id)
+      );
+    } catch (error) {
+      console.error("Error deleting usuario: ", error);
+    }
+  };
+  const handleFileChange = (e, id) => {
+    const file = e.target.files[0];
+    setSelectedFile((prevSelectedFile) => ({
+      ...prevSelectedFile,
+      [id]: file,
+    }));
+  };
+  const handleUpload = async (id) => {
+    if (selectedFile && selectedFile[id]) {
+      const file = selectedFile[id];
+      const imageUrl = await uploadFile(file);
+      setUploadedImages((prevImages) => ({ ...prevImages, [id]: imageUrl }));
+    }
+  };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevUserData) => ({ ...prevUserData, [name]: value }));
+  };
+  const handleSexoChange = (event) => {
+    const { value } = event.target;
+    setUserData((prevUserData) => ({ ...prevUserData, sexo: value }));
+  };
+  const handleRolChange = (event) => {
+    const { value } = event.target;
+    setUserData((prevUserData) => ({ ...prevUserData, rol: value }));
+  };
+  const handleDelete = (id) => {
+    setUploadedImages((prevImages) => {
+      const updatedImages = { ...prevImages };
+      delete updatedImages[id];
+      return updatedImages;
+    });
+  };
+  const enterEditMode = (id) => {
+    const usuario = usuarios.find((usuario) => usuario.id === id);
+    setEditingUserId(id);
+    setUserData(usuario);
+  };
+  const cancelEditMode = () => {
+    setEditingUserId(null);
+    resetUserData();
+  };
+  const resetUserData = () => {
+    setUserData({
+      nombre: "",
+      apellido: "",
+      fechaNacimiento: "",
+      sexo: "",
+      rol: "",
+    });
+  };
+  const isUserDataIncomplete = () => {
+    const { nombre, apellido, fechaNacimiento, sexo, rol } = userData;
+    return !nombre || !apellido || !fechaNacimiento || !sexo || !rol;
+  };
+  useEffect(() => {
+    const getUsuarios = async () => {
+      try {
+        setIsLoading(true);
+        const snapshot = await getDocs(userCollectionRef);
+        const data = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setUsuarios(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error getting usuarios: ", error);
+        setIsLoading(false);
+      }
+    };
+    getUsuarios();
+  }, []);
   return (
-    <div>
+    <div className='container'>
       <NavbarSinTexto />
       <SideBar />
+      <h1 className='perfil'>Inicio de tu perfil</h1>
+      <h2 className='datos'>Datos</h2>
+      <input
+        name='nombre'
+        placeholder='Nombre'
+        value={userData.nombre}
+        onChange={handleInputChange}
+        type='text'
+      />
+      <input
+        name='apellido'
+        placeholder='Apellido'
+        value={userData.apellido}
+        onChange={handleInputChange}
+        type='text'
+      />
+      <input
+        name='email'
+        placeholder='Email'
+        value={userData.email}
+        onChange={handleInputChange}
+        type='text'
+        disabled={editingUserId !== null}
+      />
+      <input
+        name='fechaNacimiento'
+        placeholder='Fecha de Nacimiento'
+        value={userData.fechaNacimiento}
+        onChange={handleInputChange}
+        type='date'
+      />
+      <div>
+        <select name='sexo' value={userData.sexo} onChange={handleSexoChange}>
+          <option value=''>Sexo</option>
+          <option value='hombre'>Hombre</option>
+          <option value='mujer'>Mujer</option>
+        </select>
+      </div>
+      <div>
+        <select name='rol' value={userData.rol} onChange={handleRolChange}>
+          <option value=''>Rol</option>
+          <option value='user'>User</option>
+          <option value='admin'>Admin</option>
+        </select>
+      </div>
+      <button onClick={crearUsuario}>Crear Usuario</button>
+      <div className='user-list'>
+        {usuarios.map((usuario) => (
+          <div className='user-item' key={usuario.id}>
+            <p>Nombre: {usuario.nombre}</p>
+            <p>Apellido: {usuario.apellido}</p>
+            <p>Email: {usuario.email}</p>
+            <p>Fecha de Nacimiento: {usuario.fechaNacimiento}</p>
+            <p>Sexo: {usuario.sexo}</p>
+            <p>
+              Rol:{" "}
+              {editingUserId === usuario.id ? (
+                <select
+                  name='rol'
+                  value={userData.rol}
+                  onChange={handleRolChange}
+                >
+                  <option value='user'>User</option>
+                  <option value='admin'>Admin</option>
+                </select>
+              ) : (
+                usuario.rol
+              )}
+            </p>
+            <div>
+              {editingUserId === usuario.id ? (
+                <>
+                  <label htmlFor={`file-input-${usuario.id}`}>
+                    <input
+                      id={`file-input-${usuario.id}`}
+                      type='file'
+                      onChange={(e) => handleFileChange(e, usuario.id)}
+                    />
+                  </label>
+                  {selectedFile && selectedFile[usuario.id] && (
+                    <button onClick={() => handleUpload(usuario.id)}>
+                      Subir imagen
+                    </button>
+                  )}
+                  {uploadedImages[usuario.id] && (
+                    <div>
+                      <img
+                        src={uploadedImages[usuario.id]}
+                        alt='Imagen subida'
+                      />
+                      <button onClick={() => handleDelete(usuario.id)}>
+                        Borrar imagen
+                      </button>
+                    </div>
+                  )}
+                </>
+              ) : (
+                uploadedImages[usuario.id] && (
+                  <img src={uploadedImages[usuario.id]} alt='Imagen subida' />
+                )
+              )}
+            </div>
+            <div>
+              {editingUserId === usuario.id ? (
+                <div>
+                  <button
+                    disabled={isUserDataIncomplete()}
+                    onClick={() => editarUsuario(usuario.id)}
+                  >
+                    Guardar
+                  </button>
+                  <button onClick={cancelEditMode}>Cancelar</button>
+                </div>
+              ) : (
+                <div>
+                  <button onClick={() => enterEditMode(usuario.id)}>
+                    Editar
+                  </button>
+                  <button onClick={() => borrarUsuario(usuario.id)}>
+                    Eliminar
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
-
-export default Profile;
+};
+export default Users;
