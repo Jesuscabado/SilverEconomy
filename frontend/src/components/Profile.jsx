4ce302c471b66682d33d787396c5a0995484a50b
Javier <javiergonzalez13a@gmail.com>
2023-06-13 16:11:03 +0200

    Se puede subir imagen y refrescar la pagina

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -11,6 +11,8 @@ import {
 } from "firebase/firestore";
 import { db, uploadFile } from "../Firebase";
 import "../css/Profile.css";
+import { getMetadata, getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../Firebase";
 
 const Users = () => {
   const [usuarios, setUsuarios] = useState([]);
@@ -19,18 +21,18 @@ const Users = () => {
   const [uploadedImages, setUploadedImages] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userData, setUserData] = useState({
-    nombre: '',
-    apellido: '',
-    fechaNacimiento: '',
-    sexo: '',
-    rol: ''
+    nombre: "",
+    apellido: "",
+    fechaNacimiento: "",
+    sexo: "",
+    rol: "",
   });
-  
-  const userCollectionRef = collection(db, 'usuarios');
+
+  const userCollectionRef = collection(db, "usuarios");
 
   const crearUsuario = async () => {
     if (isUserDataIncomplete()) {
-      alert('Por favor, completa todos los campos');
+      alert("Por favor, completa todos los campos");
       return;
     }
 
@@ -39,10 +41,9 @@ const Users = () => {
       setUsuarios([...usuarios, { ...userData, id: docRef.id }]);
       resetUserData();
     } catch (error) {
-      console.error('Error adding usuario: ', error);
+      console.error("Error adding usuario: ", error);
     }
   };
-  
 
   /* const signup = async (email, password, rol, nombre, apellido, fechaNacimiento, sexo) => {
     try {
@@ -60,7 +61,6 @@ const Users = () => {
     }
   }; */
 
-
   const editarUsuario = async (id) => {
     try {
       const userIndex = usuarios.findIndex((usuario) => usuario.id === id);
@@ -71,42 +71,66 @@ const Users = () => {
           ...updatedUsuarios[userIndex],
           ...updatedUserData,
         };
-        setUsuarios(updatedUsuarios); // Actualiza la lista de usuarios en el estado
-  
-        const userDocRef = doc(db, 'usuarios', id);
+        // Actualiza la lista de usuarios en el estado
+        setUsuarios(updatedUsuarios);
+
+        // Actualiza las imágenes cargadas en el estado
+        setUploadedImages((prevImages) => ({
+          ...prevImages,
+          [id]: prevImages[id] || null, // Si no hay cambios en la imagen, mantiene el valor anterior
+        }));
+        setSelectedFile((prevSelectedFile) => ({
+          ...prevSelectedFile,
+          [id]: prevSelectedFile[id] || null, // Si no hay cambios en la imagen, mantiene el valor anterior
+        }));
+
+        // Actualiza los datos en la base de datos
+        const userDocRef = doc(db, "usuarios", id);
         await updateDoc(userDocRef, updatedUserData);
         setEditingUserId(null);
         resetUserData();
       }
     } catch (error) {
-      console.error('Error al editar usuario: ', error);
+      console.error("Error al editar usuario: ", error);
     }
   };
 
   const borrarUsuario = async (id) => {
     try {
-      const userDoc = doc(db, 'usuarios', id);
+      const userDoc = doc(db, "usuarios", id);
       await deleteDoc(userDoc);
-      setUsuarios((prevUsuarios) => prevUsuarios.filter((usuario) => usuario.id !== id));
+      setUsuarios((prevUsuarios) =>
+        prevUsuarios.filter((usuario) => usuario.id !== id)
+      );
     } catch (error) {
-      console.error('Error deleting usuario: ', error);
+      console.error("Error deleting usuario: ", error);
     }
   };
 
   const handleFileChange = (e, id) => {
     const file = e.target.files[0];
-    setSelectedFile((prevSelectedFile) => ({ ...prevSelectedFile, [id]: file }));
+    setSelectedFile((prevSelectedFile) => ({
+      ...prevSelectedFile,
+      [id]: file,
+    }));
   };
 
   const handleUpload = async (id) => {
     if (selectedFile && selectedFile[id]) {
       const file = selectedFile[id];
-      const avatarPath = `avatar/${file.name}`; // Ruta dentro de la carpeta "avatar" con el ID del usuario y el nombre del archivo
+      const avatarPath = `avatar/${id}/${file.name}`; // Ruta dentro de la carpeta "avatar" con el ID del usuario y el nombre del archivo
       const imageUrl = await uploadFile(file, avatarPath); // Pasa la ruta como segundo argumento en la llamada a uploadFile
+  
+      // Guarda la URL de la imagen en Firestore
+      const userDocRef = doc(db, "usuarios", id);
+      await updateDoc(userDocRef, { avatarUrl: imageUrl });
+  
+      // Actualiza el estado de uploadedImages con la URL de la imagen
       setUploadedImages((prevImages) => ({ ...prevImages, [id]: imageUrl }));
     }
   };
   
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserData((prevUserData) => ({ ...prevUserData, [name]: value }));
@@ -116,7 +140,6 @@ const Users = () => {
     const { value } = event.target;
     setUserData((prevUserData) => ({ ...prevUserData, sexo: value }));
   };
-
 
   const handleRolChange = (event) => {
     const { value } = event.target;
@@ -144,11 +167,11 @@ const Users = () => {
 
   const resetUserData = () => {
     setUserData({
-      nombre: '',
-      apellido: '',
-      fechaNacimiento: '',
-      sexo: '',
-      rol: ''
+      nombre: "",
+      apellido: "",
+      fechaNacimiento: "",
+      sexo: "",
+      rol: "",
     });
   };
 
@@ -164,136 +187,161 @@ const Users = () => {
         const snapshot = await getDocs(userCollectionRef);
         const data = snapshot.docs.map((doc) => ({
           ...doc.data(),
-
-          id: doc.id
-
+          id: doc.id,
         }));
+  
+        // Obtén las URL de las imágenes de perfil desde Firestore
+        const uploadedImages = {};
+        for (const usuario of data) {
+          if (usuario.avatarUrl) {
+            uploadedImages[usuario.id] = usuario.avatarUrl;
+          }
+        }
+  
+        setUploadedImages(uploadedImages);
         setUsuarios(data);
         setIsLoading(false);
       } catch (error) {
-
-        console.error('Error getting usuarios: ', error);
+        console.error("Error getting usuarios: ", error);
         setIsLoading(false);
       }
     };
-
+  
     getUsuarios();
   }, []);
+  
 
   return (
     <div className='container'>
       <h1 className='perfil'>Inicio de tu perfil</h1>
       <h2 className='datos'>Datos</h2>
       <input
-        name="nombre"
-        placeholder="Nombre"
+        name='nombre'
+        placeholder='Nombre'
         value={userData.nombre}
         onChange={handleInputChange}
-        type="text"
+        type='text'
       />
       <input
-        name="apellido"
-        placeholder="Apellido"
+        name='apellido'
+        placeholder='Apellido'
         value={userData.apellido}
         onChange={handleInputChange}
-        type="text"
+        type='text'
       />
       <input
-        name="email"
-        placeholder="Email"
+        name='email'
+        placeholder='Email'
         value={userData.email}
         onChange={handleInputChange}
-        type="text"
+        type='text'
         disabled={editingUserId !== null}
       />
       <input
-        name="fechaNacimiento"
-        placeholder="Fecha de Nacimiento"
+        name='fechaNacimiento'
+        placeholder='Fecha de Nacimiento'
         value={userData.fechaNacimiento}
         onChange={handleInputChange}
-        type="date"
+        type='date'
       />
       <div>
-        <select name="sexo" value={userData.sexo} onChange={handleSexoChange}>
-          <option value="">Sexo</option>
-          <option value="hombre">Hombre</option>
-          <option value="mujer">Mujer</option>
+        <select name='sexo' value={userData.sexo} onChange={handleSexoChange}>
+          <option value=''>Sexo</option>
+          <option value='hombre'>Hombre</option>
+          <option value='mujer'>Mujer</option>
         </select>
       </div>
       <div>
-        <select name="rol" value={userData.rol} onChange={handleRolChange}>
-          <option value="">Rol</option>
-          <option value="user">User</option>
-          <option value="admin">Admin</option>
+        <select name='rol' value={userData.rol} onChange={handleRolChange}>
+          <option value=''>Rol</option>
+          <option value='user'>User</option>
+          <option value='admin'>Admin</option>
         </select>
       </div>
       <button onClick={crearUsuario}>Crear Usuario</button>
-      <div className="user-list">
+      <div className='user-list'>
         {usuarios.map((usuario) => (
-         <div className="user-item" key={usuario.id}>
-         <div className="user-info">
-           <div className="user-avatar">
-             {editingUserId === usuario.id ? (
-               <>
-                 <label htmlFor={`file-input-${usuario.id}`}>
-                   <input
-                     id={`file-input-${usuario.id}`}
-                     type="file"
-                     onChange={(e) => handleFileChange(e, usuario.id)}
-                   />
-                 </label>
-                 {selectedFile && selectedFile[usuario.id] && (
-                   <button onClick={() => handleUpload(usuario.id)}>Subir imagen</button>
-                 )}
-                 {uploadedImages[usuario.id] && (
-                   <div>
-                     <img src={uploadedImages[usuario.id]} alt="Imagen subida" />
-                     <button onClick={() => handleDelete(usuario.id)}>Borrar imagen</button>
-                   </div>
-                 )}
-               </>
-             ) : (
-               uploadedImages[usuario.id] && <img src={uploadedImages[usuario.id]} alt="Imagen subida" />
-             )}
-           </div>
-           <div className="user-details">
-             <p>Nombre: {usuario.nombre}</p>
-             <p>Apellido: {usuario.apellido}</p>
-             <p>Email: {usuario.email}</p>
-             <p>Fecha de Nacimiento: {usuario.fechaNacimiento}</p>
-             <p>Sexo: {usuario.sexo}</p>
-             <p>
-               Rol: {editingUserId === usuario.id ? (
-                 <select name="rol" value={userData.rol} onChange={handleRolChange}>
-                   <option value="user">User</option>
-                   <option value="admin">Admin</option>
-                 </select>
-               ) : (
-                 usuario.rol
-               )}
-             </p>
-           </div>
-         </div>
-         <div className="user-actions">
-           {editingUserId === usuario.id ? (
-             <div>
-               <button
-                 disabled={isUserDataIncomplete()}
-                 onClick={() => editarUsuario(usuario.id)}
-               >
-                 Guardar
-               </button>
-               <button onClick={cancelEditMode}>Cancelar</button>
-             </div>
-           ) : (
-             <div>
-               <button onClick={() => enterEditMode(usuario.id)}>Editar</button>
-               <button onClick={() => borrarUsuario(usuario.id)}>Eliminar</button>
-             </div>
-           )}
-         </div>
-       </div>
-
+          <div className='user-item' key={usuario.id}>
+            <div className='user-info'>
+              <div className='user-avatar'>
+                {editingUserId === usuario.id ? (
+                  <>
+                    <label htmlFor={`file-input-${usuario.id}`}>
+                      <input
+                        id={`file-input-${usuario.id}`}
+                        type='file'
+                        onChange={(e) => handleFileChange(e, usuario.id)}
+                      />
+                    </label>
+                    {selectedFile && selectedFile[usuario.id] && (
+                      <button onClick={() => handleUpload(usuario.id)}>
+                        Subir
+                      </button>
+                    )}
+                    {uploadedImages[usuario.id] && (
+                      <div>
+                        <img
+                          src={uploadedImages[usuario.id]}
+                          alt='Imagen subida'
+                        />
+                        <button onClick={() => handleDelete(usuario.id)}>
+                          Borrar
+                        </button>
+                      </div>
+                    )}
+                  </>
+                ) : (
+                  uploadedImages[usuario.id] && (
+                    <img src={uploadedImages[usuario.id]} alt='Imagen subida' />
+                  )
+                )}
+              </div>
+              <div className='user-details'>
+                <p>Nombre: {usuario.nombre}</p>
+                <p>Apellido: {usuario.apellido}</p>
+                <p>Email: {usuario.email}</p>
+                <p>Fecha de Nacimiento: {usuario.fechaNacimiento}</p>
+                <p>Sexo: {usuario.sexo}</p>
+                <p>
+                  Rol:{" "}
+                  {editingUserId === usuario.id ? (
+                    <select
+                      name='rol'
+                      value={userData.rol}
+                      onChange={handleRolChange}
+                    >
+                      <option value='user'>User</option>
+                      <option value='admin'>Admin</option>
+                    </select>
+                  ) : (
+                    usuario.rol
+                  )}
+                </p>
+              </div>
+            </div>
+            <div className='user-actions'>
+              {editingUserId === usuario.id ? (
+                <div>
+                  <button
+                    disabled={isUserDataIncomplete()}
+                    onClick={() => editarUsuario(usuario.id)}
+                  >
+                    Guardar
+                  </button>
+                  <button onClick={cancelEditMode}>Cancelar</button>
+                </div>
+              ) : (
+                <div>
+                  <button onClick={() => enterEditMode(usuario.id)}>
+                    Editar
+                  </button>
+                  <button onClick={() => borrarUsuario(usuario.id)}>
+                    Eliminar
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
         ))}
       </div>
     </div>
